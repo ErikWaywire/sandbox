5a69d69c2a081cb7b0068c8a3b8790ae96ec6a90
ErikWaywire <erik@waywire.com>
2013-04-28 20:03:16 -0400

    Adds passwords to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :email, :name
+  attr_accessible :bio, :email, :name, :password, :password_confirmation
+  has_secure_password
 
-  validates :name, :presence => true
-  validates :email, :presence => true
-  validates :bio, :presence => true
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, :presence => true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, :presence => true, length: { maximum: 254 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :bio, :presence => true, length: { maximum: 200 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   has_many :posts, :dependent => :destroy
 end
