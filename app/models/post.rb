4ed62c7953a6978fa516f0a1696971f436ba09b1
ErikWaywire <erik@waywire.com>
2013-05-13 00:06:52 -0400

    adds feed SQL changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body
+
   validates :body, presence: true, :length => { :maximum => 140 }
   validates :user_id, presence: true
+
   default_scope order: 'posts.created_at DESC'
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
 
   def feed
     # This is preliminary. See "Following users" for the full implementation.
-    Post.where("user_id = ?", id)
+    Post.from_users_followed_by(self)
   end
 
   def following?(other_user)
