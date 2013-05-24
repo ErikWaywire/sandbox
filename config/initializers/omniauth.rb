e50e870e28d57b803a1b9feaac543e8c20d20aef
ErikWaywire <erik@waywire.com>
2013-05-24 13:27:34 -0400

    Adds prod & test configurations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '534918226549504', 'ddedba9522f3f2d83c22d4382d0c5534', :scope => 'email,publish_actions', :display => 'popup'
+  if Rails.env.production?
+  	provider :facebook, '528803847177512', 'ea1aafd66618c9032f4467d8b4684522', :scope => 'email,publish_actions', :display => 'popup'
+  else
+  	provider :facebook, '534918226549504', 'ddedba9522f3f2d83c22d4382d0c5534', :scope => 'email,publish_actions', :display => 'popup'
+  end
   provider :twitter, 'ljbhdBR1dqMLnmkRLCjCuQ', 'cRVq03JkNNfQPM9YU2LAX5qcuPcsRIPOm4otZzFg'
 end	
